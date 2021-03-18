18686c1d32ec88b3a59306d5000e399ca3075ca3
letarzan <leandrorevolve@gmail.com>
2021-03-17 22:17:41 -0300

    Adicionando estrutura de Árvore Generica AC3

---
+++ estrutura-de-dados/ED-AC3/src/exceptions/NonEmptyTreeException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class NonEmptyTreeException extends RuntimeException {
+	public NonEmptyTreeException(String err) {
+
+		super(err);
+	}
+}
